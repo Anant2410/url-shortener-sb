e4354f2c4c6b342aa1dc94b535579e75582f062b
Anant <anantgarg2410@gmail.com>
2026-02-28 01:40:25 +0530

    jwt changes and partial implementation

---
+++ src/main/java/com/url/shortener/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,26 @@
+package com.url.shortener.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        try{
+            //get jwt from header
+            //validate the token
+            //if valid get the user details
+                // get username -> load user -> set the auth context
+        }catch (Exception e){}
+
+    }
+}
