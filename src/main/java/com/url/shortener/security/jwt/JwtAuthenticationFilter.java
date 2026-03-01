468791cf2762abd5aff073909d051ab4a5eadee3
Anant <anantgarg2410@gmail.com>
2026-03-01 10:36:25 +0530

    jwt complete authentication

--- src/main/java/com/url/shortener/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/url/shortener/security/jwt/JwtAuthenticationFilter.java
@@ -4,11 +4,28 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtils jwtTokenProvider;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+
+
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
@@ -17,10 +34,27 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         try{
             //get jwt from header
-            //validate the token
-            //if valid get the user details
-                // get username -> load user -> set the auth context
-        }catch (Exception e){}
+            String jwt = jwtTokenProvider.getJwtFromHeader(request);
 
+            //validate the token
+            if(jwt != null && jwtTokenProvider.validateToken(jwt)) {
+                String username = jwtTokenProvider.getUserNameFromJwtToken(jwt);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                //if valid get the user details
+                // get username -> load user -> set the auth context
+                if (userDetails != null) {
+                    UsernamePasswordAuthenticationToken authentication = new
+                            UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //we want to continue the filter chain else it will discontinue
+        filterChain.doFilter(request, response);
     }
 }
